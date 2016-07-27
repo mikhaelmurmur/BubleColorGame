63b849c4e1a2c891d6b970e91d423ca0e619787a
Mykhailo Stolovych <stolovich@yahoo.com>
2016-07-28 00:51:33 +0300

    Added block manager.
    Simple implementation. Need hard work.

---
+++ BubleColorGame/Assets/Scripts/Controller/CBlockController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CBlockController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position.Set(transform.position.x, transform.position.y, transform.position.z-.5f);
+    }
+}
---
+++ BubleColorGame/Assets/Scripts/Managers/CBlockManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CBlockManager : Singleton<CBlockManager>
+{
+    protected CBlockManager() { }
+    
+    [SerializeField]
+    private Transform m_spawnPoint;
+
+    [SerializeField]
+    private GameObject m_block;
+
+    [SerializeField]
+    private float deltaSpawn = 0;
+    private float currentTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentTime<deltaSpawn)
+        {
+            currentTime += Time.deltaTime;
+        }
+        else
+        {
+            currentTime -= deltaSpawn;
+            Instantiate(m_block, m_spawnPoint.position, Quaternion.identity);
+        }
+    }
+}
